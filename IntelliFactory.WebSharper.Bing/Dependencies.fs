c212e56358e243197be705d51cf3dc0693524247
atayanovskyy@2a5a2a36-3fb0-49e7-9286-111048d56d9a <atayanovskyy@2a5a2a36-3fb0-49e7-9286-111048d56d9a>
2010-11-17 12:58:56 +0000

    Updated to the latest WS.
    --HG--
    branch : 2.0
    extra : convert_revision : svn%3A2a5a2a36-3fb0-49e7-9286-111048d56d9a/branches/2.0%4018

--- IntelliFactory.WebSharper.Bing/Dependencies.fs
+++ IntelliFactory.WebSharper.Bing/Dependencies.fs
@@ -9,25 +9,18 @@
 //-----------------------------------------------------------------
 // $end{copyright}
 
-// $begin{copyright}
-//
-// This file is confidential and proprietary.
-//
-// Copyright (c) IntelliFactory, 2004-2010.
-//
-// All rights reserved.  Reproduction or use in whole or in part is
-// prohibited without the written consent of the copyright holder.
-//-----------------------------------------------------------------
-// $end{copyright}
-
 namespace IntelliFactory.WebSharper.Bing.Dependencies
 
 open IntelliFactory.WebSharper
 
 /// Requires the Bing Maps API.
 type MapsAPI() =
-    interface IResource with
-        member this.Dependencies = Seq.empty
-        member this.Id = "MS.Bing.Maps"
-        member this.Render(resolver)(writer) =
-            writer.Write("<script type=\"text/javascript\" src=\"http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.3\"></script>")
+    interface Resources.IResource with
+        member this.Id              = "MS.Bing.Maps"
+        member this.Dependencies    = Seq.empty
+        member this.Render resolver =
+            "http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.3"
+            |> Resources.RenderJavaScript
+
+[<assembly: Require(typeof<MapsAPI>)>]
+do ()
