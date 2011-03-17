245fd64fc74d0f6a072c2d07401b0fd0b3181043
tarmil <none@none>
2011-03-17 13:52:09 +0100

    Switched to wig based project
    --HG--
    branch : wig

---
+++ IntelliFactory.WebSharper.Bing/Marker.cs
@@ -0,0 +1,4 @@
+using System.Web.UI;
+[assembly: IntelliFactory.WebSharper.Resources.Require(typeof(IntelliFactory.WebSharper.JQuery.Resources.JQuery))]
+[assembly: WebResource("jquery-1.5.1.js", "text/javascript")]
+[assembly: WebResource("jquery-1.5.1.min.js", "text/javascript")]
---
+++ IntelliFactory.WebSharper.Bing/Resources.cs
@@ -0,0 +1,5 @@
+namespace IntelliFactory.WebSharper.JQuery.Resources {
+    public class JQuery : IntelliFactory.WebSharper.Resources.BaseResource {
+        public JQuery() : base("jquery-1.5.1.min.js") {}
+    }
+}
